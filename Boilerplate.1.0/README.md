e5688b287d42256890550fe5ea08927441a81e96
Heather Corey <HeatherC747@gmail.com>
2014-01-10 09:52:32 -0500

    Add boilerplate folder

---
+++ Boilerplate.1.0/README.md
@@ -0,0 +1,4 @@
+Boilerplate.1.0
+===============
+
+HubSpot COS Boilerplate 1.0 Stylesheet and Required Files
---
+++ Boilerplate.1.0/styles/company-main.js
@@ -0,0 +1,48 @@
+$(function() {
+
+    /** 
+     * Mobile Nav
+     *
+     * Hubspot Standard Toggle Menu
+     */
+
+    $('.custom-menu-primary').addClass('js-enabled');
+    $('.custom-menu-primary .hs-menu-flow-horizontal').before('<a class="mobile-trigger"><span></span></a>');
+    $('.custom-menu-primary .hs-item-has-children > a').after('<a class="child-trigger"><span></span></a>');
+    $('a.mobile-trigger').click(function() {
+        $(this).next('.custom-menu-primary .hs-menu-flow-horizontal').slideToggle(250);
+        $('body').toggleClass('mobile-open');
+        $('a.child-trigger').removeClass('child-open');
+        $('.hs-menu-children-wrapper').hide(250);
+        return false;
+     });
+
+    $('a.child-trigger').click(function() {
+        $(this).parent().siblings('.hs-item-has-children').find('a.child-trigger').removeClass('child-open');
+        $(this).parent().siblings('.hs-item-has-children').find('.hs-menu-children-wrapper').hide(250);
+        $(this).next('.hs-menu-children-wrapper').slideToggle(250);
+        $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('.hs-menu-children-wrapper').hide(250);
+        $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('a.child-trigger').removeClass('child-open');
+        $(this).toggleClass('child-open');
+        return false;
+    });
+
+    /** 
+     * Add Custom Javascript Below
+     */
+
+});
+
+
+
+
+
+/*
+[hubspot-metadata]
+{
+    "category": "page", 
+    "creatable": false, 
+    "path": "custom/page/company-theme/company-main.js"
+}
+[end-hubspot-metadata]
+*/
