a9e48a8987620129660a668bf746bc5390ed7b1e
Colby Schneider <cschneider@hubspot.com>
2014-01-27 11:03:12 -0500

    Make child menus slide up on close.
    Changes to Mobile Menu JS to have child menus slide up when closing
    rather than to the left.

--- Boilerplate.1.0/styles/CompanyMain.js
+++ Boilerplate.1.0/styles/CompanyMain.js
@@ -7,25 +7,26 @@ $(function() {
      */
 
     $('.custom-menu-primary').addClass('js-enabled');
-    $('.custom-menu-primary .hs-menu-flow-horizontal').before('<a class="mobile-trigger"><span></span></a>');
+    $('.custom-menu-primary .hs-menu-flow-horizontal').before('<a class="mobile-trigger"><span></span><i></i></a>');
     $('.custom-menu-primary .hs-item-has-children > a').after('<a class="child-trigger"><span></span></a>');
     $('a.mobile-trigger').click(function() {
         $(this).next('.custom-menu-primary .hs-menu-flow-horizontal').slideToggle(250);
         $('body').toggleClass('mobile-open');
         $('a.child-trigger').removeClass('child-open');
-        $('.hs-menu-children-wrapper').hide(250);
+        $('.hs-menu-children-wrapper').slideUp(250);
         return false;
      });
 
     $('a.child-trigger').click(function() {
         $(this).parent().siblings('.hs-item-has-children').find('a.child-trigger').removeClass('child-open');
-        $(this).parent().siblings('.hs-item-has-children').find('.hs-menu-children-wrapper').hide(250);
+        $(this).parent().siblings('.hs-item-has-children').find('.hs-menu-children-wrapper').slideUp(250);
         $(this).next('.hs-menu-children-wrapper').slideToggle(250);
-        $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('.hs-menu-children-wrapper').hide(250);
+        $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('.hs-menu-children-wrapper').slideUp(250);
         $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('a.child-trigger').removeClass('child-open');
         $(this).toggleClass('child-open');
         return false;
     });
+
 
     /** 
      * Add Custom Javascript Below
