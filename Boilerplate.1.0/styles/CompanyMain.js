bd13464afdbce3fbfc1290a6413b5656c5a1a558
captshawn <captshawn@mac.com>
2014-04-15 11:41:22 -0400

    General Updates 1.2.1

--- Boilerplate.1.0/styles/CompanyMain.js
+++ Boilerplate.1.0/styles/CompanyMain.js
@@ -7,29 +7,24 @@ $(function() {
      */
 
     $('.custom-menu-primary').addClass('js-enabled');
-    $('.custom-menu-primary .hs-menu-flow-horizontal').before('<a class="mobile-trigger"><span></span><i></i></a>');
-    $('.custom-menu-primary .flyouts .hs-item-has-children > a').after('<a class="child-trigger"><span></span></a>');
-    $('a.mobile-trigger').click(function() {
-        $(this).next('.custom-menu-primary .hs-menu-flow-horizontal').slideToggle(250);
+    $('.custom-menu-primary .hs-menu-wrapper').before('<div class="mobile-trigger"><span></span><i></i></div>');
+    $('.custom-menu-primary .flyouts .hs-item-has-children > a').after(' <div class="child-trigger"><span></span></div>');
+    $('.mobile-trigger').click(function() {
+        $(this).next('.custom-menu-primary .hs-menu-wrapper').slideToggle(250);
         $('body').toggleClass('mobile-open');
-        $('a.child-trigger').removeClass('child-open');
+        $('.child-trigger').removeClass('child-open');
         $('.hs-menu-children-wrapper').slideUp(250);
         return false;
      });
 
-    $('a.child-trigger').click(function() {
-        $(this).parent().siblings('.hs-item-has-children').find('a.child-trigger').removeClass('child-open');
+    $('.child-trigger').click(function() {
+        $(this).parent().siblings('.hs-item-has-children').find('.child-trigger').removeClass('child-open');
         $(this).parent().siblings('.hs-item-has-children').find('.hs-menu-children-wrapper').slideUp(250);
         $(this).next('.hs-menu-children-wrapper').slideToggle(250);
         $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('.hs-menu-children-wrapper').slideUp(250);
-        $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('a.child-trigger').removeClass('child-open');
+        $(this).next('.hs-menu-children-wrapper').children('.hs-item-has-children').find('.child-trigger').removeClass('child-open');
         $(this).toggleClass('child-open');
         return false;
     });
-
-
-    /** 
-     * Add Custom Javascript Below
-     */
 
 });
